43c961320f7a8137bfceda87b67bc18043ebc650
Nikhil <nikhilpatravale2373@gmail.com>
2023-01-06 17:55:37 +0530

    added message routes

--- api/routes/Messages.js
+++ api/routes/Messages.js
@@ -1,6 +1,7 @@
 const {Router} = require('express');
 const router = Router();
 const MessageModel = require('../models/Message');
+const UserModel = require("../models/Users")
 
 router.post("/", async (req, res) => {
     try{
@@ -12,8 +13,19 @@ router.post("/", async (req, res) => {
     }
 })
 
-router.get("/inbox", (req, res) => {
+router.get("/inbox/:empId", async (req, res) => {
+    try{
+        const id = Number(req.params.empId)
+        const user = await UserModel.findOne({empId: id})
+        console.log(user)
+        const messages = await MessageModel.find({
+            to: { $in: [user.email]}
+        })
 
+        return res.status(200).json(messages)
+    }catch(err){
+        res.status(500).json(err)
+    }
 })
 
 module.exports = router
